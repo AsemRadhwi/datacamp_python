d7b33cf75c227382c84fec8ba2bb48e6ba8a24a8
AsemRadhwi <asem.radhwi@gmail.com>
2019-02-15 18:17:37 +0300

    lab2

--- np_and_pd.py
+++ np_and_pd.py
@@ -59,3 +59,45 @@ print(type(recent_grads_np))
 # Calculate correlation matrix
 print(np.corrcoef(recent_grads_np[:,0], recent_grads_np[:,1]))
 
+# Add sharemen column
+recent_grads['sharemen'] = recent_grads['men'] / recent_grads['total']
+
+# Find the maximum percentage value of men
+max_men = np.max(recent_grads['sharemen'])
+ 
+# Output the row with the highest percentage of men
+print(recent_grads[recent_grads['sharemen']==max_men])
+
+# Add gender_diff column
+recent_grads['gender_diff'] = recent_grads['sharewomen'] - recent_grads['sharemen'] 
+
+# Make all gender difference values positive
+recent_grads['gender_diff'] = recent_grads['gender_diff'].abs()
+
+# Find the 5 rows with lowest gender rate difference
+print(recent_grads.nsmallest(5, 'gender_diff'))
+
+# Rows where gender rate difference is greater than .30 
+diff_30 = recent_grads['gender_diff'] > .30
+
+# Rows with more men
+more_men = recent_grads['men'] > recent_grads['women']
+
+# Combine more_men and diff_30
+more_men_and_diff_30 = np.logical_and(diff_30,more_men)
+
+# Find rows with more men and and gender rate difference greater than .30
+fewer_women = recent_grads[more_men_and_diff_30]
+
+# Group by major category and count
+print(recent_grads.groupby(['major_category']).major_category.count())
+
+# Group departments that have less women by category and count
+print(fewer_women.groupby('major_category').major_category.count())
+
+# Report average gender difference by major category
+print(recent_grads.groupby('major_category').gender_diff.mean())
+
+# Find average number of low wage jobs and unemployment rate of each major category
+dept_stats = recent_grads.groupby(['major_category'])['low_wage_jobs', 'unemployment_rate'].mean()
+print(dept_stats)
